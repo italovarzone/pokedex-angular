ba9a6c751b41fb41c071a7a66875fdc218da98ce
ipv <ipv@vimansca.local>
2023-11-08 11:32:04 -0300

    Adiciona as rotas das paginas e o lazy loading das paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Modules
+import { PagesModule } from './pages/pages.module';
+
+//Components
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, PagesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
