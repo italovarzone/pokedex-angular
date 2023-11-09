5ba045078bbcd164bcca97dad59ecaf3a3b3b9e0
ipv <ipv@vimansca.local>
2023-11-09 13:56:11 -0300

    Adiciona o html e o css da pagina details e tratativas de erros da API

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { forkJoin } from 'rxjs';
 
-// Services
+//Services
 import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
@@ -14,24 +14,34 @@ export class DetailsComponent implements OnInit {
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
   constructor(
-    private ActivatedRoute: ActivatedRoute,
-    private PokeApiService: PokeApiService
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
   ) {}
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon();
   }
 
-  get pokemon() {
-    const id = this.ActivatedRoute.snapshot.params['id'];
-    const pokemon = this.PokeApiService.apiGetPokemon(
+  public getPokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemon(
       `${this.urlPokemon}/${id}`
     );
-    const name = this.PokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
 
-    return forkJoin([pokemon, name]).subscribe(res => {
-      console.log(res);
-    });
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
+      }
+    );
   }
 }
