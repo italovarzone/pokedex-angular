70207394454f3ed16b784cff4e446897aaf718a0
ipv <ipv@vimansca.local>
2023-11-09 13:26:55 -0300

    Configura para na pagina details conseguir receber os dados da ApiService

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+// Services
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent {}
+export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private PokeApiService: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon() {
+    const id = this.ActivatedRoute.snapshot.params['id'];
+    const pokemon = this.PokeApiService.apiGetPokemon(
+      `${this.urlPokemon}/${id}`
+    );
+    const name = this.PokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(res => {
+      console.log(res);
+    });
+  }
+}
