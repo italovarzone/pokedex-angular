8fb37573621a19bc0afb2a7c1bc6fe4475c2d93e
ipv <ipv@vimansca.local>
2023-11-10 15:05:27 -0300

    Auto-generated commit [ci skip]

--- src/app/shared/poke-header/poke-header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-poke-header',
-  templateUrl: './poke-header.component.html',
-  styleUrls: ['./poke-header.component.scss'],
-})
-export class PokeHeaderComponent {}
